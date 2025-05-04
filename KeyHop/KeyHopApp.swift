b3ce30b1e339fd5d0aae1caa5044e8151d869739
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-04 14:40:56 +0900

    Revert "remove trailing spaces"
    This reverts commit f681109699c7ec0c655e7a28ff709de56c240dd0.

--- KeyHop/KeyHopApp.swift
+++ KeyHop/KeyHopApp.swift
@@ -1,1 +1,29 @@
-import SwiftUIimport SwiftData@mainstruct KeyHopApp: App {    var sharedModelContainer: ModelContainer = {        let schema = Schema([            KeybindingsData.self,        ])        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)        do {            return try ModelContainer(for: schema, configurations: [modelConfiguration])        } catch {            fatalError("Could not create ModelContainer: \(error)")        }    }()    init() {        HotkeyManager.shared.start()    }    var body: some Scene {        WindowGroup {            ContentView()        }        .modelContainer(sharedModelContainer)    }}
+import SwiftUI
+import SwiftData
+
+@main
+struct KeyHopApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            KeybindingsData.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    init() {
+        HotkeyManager.shared.start()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
--- KeyHop/KeybindingsData.swift
+++ KeyHop/KeybindingsData.swift
@@ -1,1 +1,13 @@
-import Foundationimport SwiftData@Modelfinal class KeybindingsData {    var applicationPath: String    var keybindings: String    init(applicationPath: String, keybindings: String) {        self.applicationPath = applicationPath        self.keybindings = keybindings    }}
+import Foundation
+import SwiftData
+
+@Model
+final class KeybindingsData {
+    var applicationPath: String
+    var keybindings: String
+    
+    init(applicationPath: String, keybindings: String) {
+        self.applicationPath = applicationPath
+        self.keybindings = keybindings
+    }
+}
