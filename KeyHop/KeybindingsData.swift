e925c180be5ce3b534b393b8badc91ee84c0f8a6
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-04 14:34:30 +0900

    remove trailing spaces

--- KeyHop/KeybindingsData.swift
+++ KeyHop/KeybindingsData.swift
@@ -1,13 +1,1 @@
-import Foundation
-import SwiftData
-
-@Model
-final class KeybindingsData {
-    var applicationPath: String
-    var keybindings: String
-    
-    init(applicationPath: String, keybindings: String) {
-        self.applicationPath = applicationPath
-        self.keybindings = keybindings
-    }
-}
+import Foundationimport SwiftData@Modelfinal class KeybindingsData {    var applicationPath: String    var keybindings: String    init(applicationPath: String, keybindings: String) {        self.applicationPath = applicationPath        self.keybindings = keybindings    }}
